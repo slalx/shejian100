c91bb97d31a1dbfb8e6877a02c925f80fa91f71a
slalx@126.com <63114178>
2013-01-20 01:01:41 +0800

    add db file

--- index.php
+++ index.php
@@ -8,23 +8,33 @@
 	<div>我是张胜利</div>
 <?php 
 
-echo '我在php代码中开始连接数据库\n';
+echo '我在php代码中开始连接数据库<br>';
 
-echo "定义数据库的变量\n";
+echo "定义数据库的变量<br>";
 DEFINE('DB_HOST','42.96.139.171');
 DEFINE('DB_USER','root');
 DEFINE('DB_PASSWORD','41d16801c7');
 DEFINE('DB_NAME','shejian');
 
-echo "开始连接数据库\n";
+echo "开始连接数据库<br>";
 $dbc = @mysql_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) OR die('not conn mysql:'.mysql_connect_error() );
 printf("connction success <br>");
 
-echo '我在php代码中连接数据库结束\n';
+echo '我在php代码中连接数据库结束<br>';
+
+$name = '麻辣香锅'/*$_POST["name"]*/;
+echo $name."我传第的名字<br>";
+/* 插入*/
 
 
-include 'http://42.96.139.171/db/db_close.php';
+$q = "insert into restaurant(name) VALUES ('".$name."')";
+echo $q."sql语句<br>";
+$r = mysql_query($dbc,$q);
+printf("insert success<br>");
 
+
+
+mysql_close($dbc);
 ?>
 </body>
 </html>
