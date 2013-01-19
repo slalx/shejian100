b1b56cbe218998237aa024b1f6413ee0e79d3343
slalx@126.com <63114178>
2013-01-19 23:29:04 +0800

    add db file

--- db/db_open.php
+++ db/db_open.php
@@ -1,25 +1,26 @@
-<!doctype html>
-<html >
-<head>
-<meta charset="utf-8" />
-<title></title>
-</head>
-<body>
 <?php 
 
+DEFINE('DB_HOST','42.96.139.171');
+DEFINE('DB_USER','root');
+DEFINE('DB_PASSWORD','41d16801c7');
+DEFINE('DB_NAME','shejian');
 
-require '/db/db_open.php';
+$dbc = @mysql_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) OR die('not conn mysql:'.mysql_connect_error() );
+printf("connction success <br>");
 
 /*$name = $_POST["name"];
-echo $name."我传第的名字<br>";
-// 插入
+echo $name."我传第的名字<br>";*/
+/* 插入*/
+
+/*
 $q = "insert into restaurant(name) VALUES ('".$name."')";
 echo $q."sql语句<br>";
 $r = mysqli_query($dbc,$q);
 printf("insert success<br>");
+*/
 
-// 查询
-$qq = "select * from restaurant";
+/* 查询*/
+/*$qq = "select * from restaurant";
 $result = mysqli_query($dbc,$qq);
 
 if( $result ){
@@ -29,30 +30,24 @@ if( $result ){
 	}
 }else{
 	echo "why no result<br>";
-}
+}*/
 
-// 修改
-$qu = "update restaurant set name='guolin' where id=1";
+/* 修改*/
+/*$qu = "update restaurant set name='guolin' where id=1";
 $result = mysqli_query($dbc,$qu);
 if($result){
 	echo "updata success<br>";
 }else{
 	echo "updata fail<br>";
-}
+}*/
 
-// 删除
-$qu = "delete from restaurant where id=6";
+/* 删除*/
+/*$qu = "delete from restaurant where id=6";
 $result = mysqli_query($dbc,$qu);
 if($result){
 	echo "delete success<br>";
 }else{
 	echo "delete fail<br>";
-}
-*/
-
-require '/db/db_close.php';
-
+}*/
 ?>
-</body>
-</html>
 
