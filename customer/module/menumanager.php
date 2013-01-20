6a9af8c881c30dc1d8676fa965519c990d5b0f9e
slalx@126.com <63114178>
2013-01-20 17:27:08 +0800

    add customer

--- customer/module/menumanager.php
+++ customer/module/menumanager.php
@@ -21,8 +21,8 @@
 				<div class="right title opt">操作</div> 
 			</div> 
 			<ul id="listContainer"> 
-				<li class="listItem buddyRichInfoC"> <div class="left"> <input class="chooseFriend" type="checkbox" value="983031980"> </div> <a target="_blank" href="#" class="msgSenderAvatar left"> <img height="48" width="48" src="/cgi-bin/getheadimg?fakeid=983031980" data-fakeid="983031980" class="avatar left"> </a> <a target="_blank" href="/cgi-bin/singlemsgpage?fromfakeid=983031980&amp;msgid=&amp;source=&amp;count=20&amp;t=wxm-singlechat&amp;lang=zh_CN" class="msgSender f16 c-l b left" data-fakeid="983031980">酸辣土豆丝</a> <span class="remarkName left" data-fakeid="983031980"></span> <div class="right"> <button class="msgSenderRemark right btnGrayS" data-fakeid="983031980">修改</button> <button data-gid="0" data-fid="983031980" class="putIntoGroup btnGrayS right">12</button>  <div class="clr"></div> </div> <div class="clr"></div> </li> 
-				<li class="listItem buddyRichInfoC"> <div class="left"> <input class="chooseFriend" type="checkbox" value="2412503920"> </div> <a target="_blank" href="#" class="msgSenderAvatar left"> <img height="48" width="48" src="/cgi-bin/getheadimg?fakeid=2412503920" data-fakeid="2412503920" class="avatar left"> </a> <a target="_blank" href="/cgi-bin/singlemsgpage?fromfakeid=2412503920&amp;msgid=&amp;source=&amp;count=20&amp;t=wxm-singlechat&amp;lang=zh_CN" class="msgSender f16 c-l b left" data-fakeid="2412503920">蒜蓉油麦菜</a> <span class="remarkName left" data-fakeid="2412503920"></span> <div class="right"> <button class="msgSenderRemark right btnGrayS" data-fakeid="2412503920">修改</button> <button data-gid="0" data-fid="2412503920" class="putIntoGroup btnGrayS right">30</button>  <div class="clr"></div> </div> <div class="clr"></div> </li> 
+				<li class="listItem buddyRichInfoC"> <div class="left"> <input class="chooseFriend" type="checkbox" value="983031980"> </div>  <a target="_blank" href="#" class="msgSender f16 c-l b left" data-fakeid="983031980">酸辣土豆丝</a> <span class="remarkName left" data-fakeid="983031980"></span> <div class="right"> <button class="msgSenderRemark right btnGrayS" data-fakeid="983031980">修改</button> <button data-gid="0" data-fid="983031980" class="putIntoGroup btnGrayS right">12</button>  <div class="clr"></div> </div> <div class="clr"></div> </li> 
+				<li class="listItem buddyRichInfoC"> <div class="left"> <input class="chooseFriend" type="checkbox" value="2412503920"> </div> <a target="_blank" href="#" class="msgSender f16 c-l b left" data-fakeid="2412503920">蒜蓉油麦菜</a> <span class="remarkName left" data-fakeid="2412503920"></span> <div class="right"> <button class="msgSenderRemark right btnGrayS" data-fakeid="2412503920">修改</button> <button data-gid="0" data-fid="2412503920" class="putIntoGroup btnGrayS right">30</button>  <div class="clr"></div> </div> <div class="clr"></div> </li> 
 			</ul> 
 			<div class="cLine"> 
 				<div class="pageNavigator right"> 
@@ -39,8 +39,7 @@
 					<li class="selected "> <a href="#">全部</a> </li> 
 					<li class=" "> <a href="#">热菜</a> </li> 
 					<li class=" "> <a href="#">凉菜</a> </li> 
-					<li id="groupAdd" class="group groupAdd"><a class="icon18C iconAdd" href="javascript:;">新建分组</a></li>
-					<li id="friendSearch" class="selected none"> <span>搜索结果</span> </li> 
+					<li id="groupAdd" class="group groupAdd"><a class="icon18C iconAdd" href="javascript:;">添加新菜</a></li>
 				</ul> 
 			</div> 
 		</div> 
