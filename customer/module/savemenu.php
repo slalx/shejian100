a5612459b854a509cd4ed80610ab30ee150040b0
slalx@126.com <63114178>
2013-01-20 21:34:11 +0800

    add menu

--- customer/module/savemenu.php
+++ customer/module/savemenu.php
@@ -4,11 +4,13 @@
 include '../db/db_open.php';
 
 for ($i=0;$i<count($_POST["name"]);$i++){
+	echo "插入第".$i."条记录";
 	if ($_POST["name"][$i]!=''){
 		$sql = "insert into dish(name,price,type,restaurantid,userid) values (‘$_POST[dish_name][$i]’,$_POST[dish_price][$i],$_POST[dish_type][$i],10,10)";
 		mysql_query($sql);
 		echo "插入第".$i."条记录";
 	}
+	echo "插入第".$i."条记录";
 }
 
 include '../db/db_close.php';
