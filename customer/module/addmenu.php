4739637e2fd081bc500a715417898801a2451433
slalx@126.com <63114178>
2013-01-20 20:53:31 +0800

    add menu

--- customer/module/addmenu.php
+++ customer/module/addmenu.php
@@ -1,3 +1,9 @@
+<style type="text/css">
+	.dish_item{
+		padding-bottom: 23px;
+	}
+</style>
+
 
 <div id="main" class="container">
     <div class="containerBox boxIndex"> 
@@ -9,12 +15,29 @@
 				<div class="msg-edit-area" id="msgEditArea" style="margin-top: 31px; "> 
 					<div class="rel msg-editer-wrapper"> 
 						<div class="msg-editer"> 
-							<div>
+							<div class="dish_item">
 								<label for="">菜名</label> 
 								<input type="text" class="msg-input" value=""> 
-								<label for="">价格</label>
+								<label for="" class="dish_price">价格</label>
 								<input type="text" class="msg-input" value="">
+								<label for="">种类</label> 
+								<select name="dish_type">
+									<option value="1">热菜</option>
+									<option value="2">凉菜</option>
+								</select>
 							</div>
+							<div class="dish_item">
+								<label for="">菜名</label> 
+								<input type="text" class="msg-input" value=""> 
+								<label for="" class="dish_price">价格</label>
+								<input type="text" class="msg-input" value="">
+								<label for="">种类</label> 
+								<select name="dish_type">
+									<option value="1">热菜</option>
+									<option value="2">凉菜</option>
+								</select>
+							</div>
+
 						</div> 
 						<div class="oh z shadow"> 
 							<span class="left ls"></span>
