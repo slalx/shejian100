35310113c65c375b89cc9718659ad1914a21d5d0
slalx@126.com <63114178>
2013-01-20 20:17:01 +0800

    add customer

---
+++ customer/module/addmenu.php
@@ -0,0 +1,48 @@
+
+<div id="main" class="container">
+    <div class="containerBox boxIndex"> 
+		<div class="boxHeader"> 
+			<h2>添加菜单</h2> 
+		</div> 
+		<div class="content"> 
+			<div class="z oh msg-edit"> 
+				<div class="msg-edit-area" id="msgEditArea" style="margin-top: 31px; "> 
+					<div class="rel msg-editer-wrapper"> 
+						<div class="msg-editer"> 
+							<label for="" class="block">标题</label> 
+							<input type="text" class="msg-input" id="title" value="爱的发声的发生地"> 
+							<label for="" class="block">封面</label> 
+							<a id="desc-block-link" href="javascript:(function(){jQuery('#desc-block').show();jQuery('#desc-block-link').hide()})()" class="url-link block" style="display: none; ">添加摘要</a> 
+							<label for="" class="block">正文</label> 
+							<a id="url-block-link" href="javascript:(function(){jQuery('#url-block').show();jQuery('#url-block-link').hide()})()" class="url-link" style="display: none; ">添加来源</a> 
+							<div id="url-block" class="none" style="display: block; "> 
+								<label for="" class="block">来源</label> 
+								<input type="text" class="msg-input" id="url" value=""> 
+							</div> 
+						</div> 
+						<div class="oh z shadow"> 
+							<span class="left ls"></span>
+							<span class="right rs"></span> 
+						</div> 
+						<span class="abs msg-arrow a-out" style="margin-top: 0px; "></span> 
+						<span class="abs msg-arrow a-in" style="margin-top: 0px; "></span> 
+					</div> 
+				</div> 
+			</div> 
+			<p class="tc msg-btn"> 
+				<a href="javascript:;" id="previewAppMsg" class="btnGreen">发送预览</a> 
+				<a href="javascript:;" id="save" class="btnGreen">完成</a> 
+			</p> 
+		</div>
+		<div class="clr"></div>
+	</div>
+</div>
+
+
+
+
+
+
+
+
+
