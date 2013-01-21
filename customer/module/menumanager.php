dc1e22ea01075b66da3a52905fd986751d01db6d
slalx@126.com <63114178>
2013-01-21 15:53:55 +0800

    add menu

--- customer/module/menumanager.php
+++ customer/module/menumanager.php
@@ -1,17 +1,20 @@
 
 <?php
-echo "0";
+
 include '../db/db_open.php';
-echo "1";
+
 //设置每一页显示的记录数
   $pagesize = 3; 
-echo "2";
+
 //取得记录总数$rs，计算总页数用
-　$rs = mysql_query("select count(*) from dish");echo "3";
-  $myrow = mysql_fetch_array($rs);echo "4";
-　$numrows = $myrow[0];echo "5";
+
+
+  
+　$rs = mysql_query("select count(*) from dish");
+  $myrow = mysql_fetch_array($rs);
+　$numrows = $myrow[0];
 //计算总页数
-　$pages = intval($numrows/$pagesize);echo "6";
+　$pages = intval($numrows/$pagesize);
 　if ($numrows%$pagesize)
 　　$pages++;
 //设置页数
