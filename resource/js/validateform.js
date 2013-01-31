6d97904fd6c2c0bf565efa7ecc65030bf7e4bd5a
slalx@126.com <63114178>
2013-01-31 16:35:29 +0800

    add file

--- resource/js/validateform.js
+++ resource/js/validateform.js
@@ -3,7 +3,7 @@ var ValidateForm = {
 
 	 validateForm: function(formid){
 	 	var formobj = document.getElementById(formid);
-	 	var inputs = formobj.getElementsByClassName('msg-input');
+	 	var inputs = ValidateForm.getElementsByClassName('msg-input',formobj,'*');
 	 	for(var i=0,l=inputs.length; i<l; i++){
 	 		var inputobj = inputs[i];
 	 		var regex = inputobj.getAttribute('data-regex');
@@ -25,6 +25,42 @@ var ValidateForm = {
 	 },
 	 hideTip: function hideTip(){
 	 	document.getElementById('messagetip').style.display="none";
-	 }
+	 },
+	 getElementsByClassName : function (searchClass, node,tag) {
+	  if(document.getElementsByClassName){
+	    var nodes =  (node || document).getElementsByClassName(searchClass),result = [];
+	      for(var i=0 ;node = nodes[i++];){
+	        if(tag !== "*" && node.tagName === tag.toUpperCase()){
+	          result.push(node)
+	        }else{
+	          result.push(node)
+	        }
+	      }
+	      return result
+	    }else{
+	      node = node || document;
+	      tag = tag || "*";
+	      var classes = searchClass.split(" "),
+	      elements = (tag === "*" && node.all)? node.all : node.getElementsByTagName(tag),
+	      patterns = [],
+	      current,
+	      match;
+	      var i = classes.length;
+	      while(--i >= 0){
+	        patterns.push(new RegExp("(^|\\s)" + classes[i] + "(\\s|$)"));
+	      }
+	      var j = elements.length;
+	      while(--j >= 0){
+	        current = elements[j];
+	        match = false;
+	        for(var k=0, kl=patterns.length; k<kl; k++){
+	          match = patterns[k].test(current.className);
+	          if (!match)  break;
+	        }
+	        if (match)  result.push(current);
+	      }
+	      return result;
+	    }
+  	}
 
 }
